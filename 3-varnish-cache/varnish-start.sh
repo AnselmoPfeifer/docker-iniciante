444e42ee57fadba65c39e1c52b0251a164e69a9c
Anselmo Pfeifer <anselmo.pfeifer@tecsinapse.com.br>
2016-11-12 14:18:31 -0300

    Ajuste de docker-compose

--- 3-varnish-cache/varnish-start.sh
+++ 3-varnish-cache/varnish-start.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-docker pull ubuntu:16.10
-docker build -t ubuntu:16.10 .
+#docker pull anspfeifer/varnish:1.0
+#docker build -t anspfeifer/varnish:1.0 .
 
 #docker run -it --name varnish -p 6081:6081 ubuntu:16.10
-docker run -it --name varnish --link tomcat1:tomcat1 --link tomcat2:tomcat2 --link tomcat3:tomcat3 -p 6081:6081 ubuntu:16.10
+docker run -it --name varnish --link tomcat1:tomcat1 --link tomcat2:tomcat2 --link tomcat3:tomcat3 -p 6081:6081 anspfeifer/varnish:1.0
+
+#docker run -it --name varnish --link tomcat1:tomcat1 --link tomcat2:tomcat2 --link tomcat3:tomcat3 -p 6081:6081 ubuntu:16.10
 
 # docker cp varnish varnish:/etc/default/varnish
 # docker cp default.vcl varnish:/etc/varnish/default.vcl
